2e06fbb5610dfc59a53519a237b3c7f4693aa473
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-04-07 12:57:15 +0530

    Added:Stack Logic

--- src/com/bridgelabz/stackqueue/Main.java
+++ src/com/bridgelabz/stackqueue/Main.java
@@ -1,10 +1,20 @@
+/*@Purpose::Ability to create a Stack of 56->30->70
+ *@File::Stack_Queue
+ *@Author::Kishlay Kishan
+ *@Date::06-April-2022
+ */
+
 package com.bridgelabz.stackqueue;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Stack and queue");
+		Stack stack = new Stack();
+		stack.push(70);
+		stack.push(30);
+		stack.push(56);
+		stack.displayStack();
 
 	}
-
 }
